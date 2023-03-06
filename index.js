7a517b2580193d6029d02ce9ce3458e292dc571e
Gabriel_FS13 <falcaoshadowbr@gmail.com>
2023-03-06 12:53:41 -0300

    improved prompt

--- index.js
+++ index.js
@@ -14,61 +14,55 @@ const config = new Configuration({
 
 const openai = new OpenAIApi(config)
 
-async function pegaEmoji(sinopse, title){
-
+async function pegaEmoji(overview, title){
     const resposta = await openai.createCompletion({
         model: "text-davinci-003",
-        prompt: `Você vai receber um título de um filme e a sinopse, e deve traduzir para emojis: sinopse: ${sinopse}, título: ${title}.`,
+        prompt: `You will receive a title and overview of a movie, you have to transform both of them to ONLY EMOJIS: title: ${title} overview: ${overview}`,
         max_tokens: 100,
-        temperature: 0.8
+        temperature: 0.5
     }); 
-
     return resposta.data.choices[0].text
-    
 }
 
 async function pegaFilme(genero_id){
     const movieID = Math.floor(Math.random() * 50 + 1)
-    const movieIndex = Math.floor(Math.random() * 19 + 1)
+    const movieIndex = Math.floor(Math.random() * 20 + 1)
     if(genero_id){
-        conexaoString = `https://api.themoviedb.org/3/discover/movie?api_key=${MOVIE_KEY}&language=pt-BR&page=${movieID}&with_genres=${genero_id}`
+        conexaoString = `https://api.themoviedb.org/3/discover/movie?api_key=${MOVIE_KEY}&language=en-US&page=${movieID}&with_genres=${genero_id}`
     }else{
-        conexaoString = `https://api.themoviedb.org/3/discover/movie?api_key=${MOVIE_KEY}&language=pt-BR&page=${movieID}`
+        conexaoString = `https://api.themoviedb.org/3/discover/movie?api_key=${MOVIE_KEY}&language=en-US&page=${movieID}`
     }
     const conexao = await fetch(conexaoString).catch(err => console.log(err))
     const dados = await conexao.json()
     return dados.results[movieIndex]
 }
 
-async function pegaIdFilme(id){
+async function traduzFilme(id){
+    const conn = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${MOVIE_KEY}&language=pt-BR`).catch(err => console.log(err))
+    const data = await conn.json()
 
-    const conexao = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${MOVIE_KEY}&language=pt-BR`)
-    const data = await conexao.json()
-
-    return data.genres
-
+    return data
 }
 
 async function montaQuiz(gen_id = false){
     var filme = await pegaFilme(gen_id)
-    var emojis = await pegaEmoji(filme.overview, filme.title) 
-    var genre = await pegaIdFilme(filme.id)
- 
-    while(emojis === "\n\n🤷‍♂️" || emojis === "\n\n❓" || emojis == "\n\n🤔🤷‍♂️🤷‍♀️" || filme.overview === ''){
+    var emojis = await pegaEmoji(filme.overview, filme.original_title) 
+    var traduzido = await traduzFilme(filme.id)
+    while(emojis === "\n\n🤷‍♂️" || emojis === "\n\n❓" || emojis === "\n\n🤔🤷‍♂️🤷‍♀️" || traduzido.overview === ''){
         filme = await pegaFilme(gen_id)
-        emojis = await pegaEmoji(filme.overview, filme.title)
-        genre = await pegaIdFilme(filme.id)
+        emojis = await pegaEmoji(filme.overview, filme.original_title)
+        traduzido = await traduzFilme(filme.id)
+
     }
-
-
-    return {emojis, 
+ 
+    return {
+        emojis, 
             hints: {
             release_date: filme.release_date,
-            overview: filme.overview.slice(0, 50),
-            genre: genre
+            overview: traduzido.overview.slice(0, 50) + "...",
         },
         poster: 'https://image.tmdb.org/t/p/original/'+filme.poster_path,
-        answer: filme.title,
+        answer: traduzido.title,
         original: filme.original_title
     }
 }
@@ -76,11 +70,11 @@ async function montaQuiz(gen_id = false){
 app.use(cors())
 
 
-app.get('/',cors(), async (req, res) =>{
+app.get('/', cors(), async (req, res) =>{
     res.json(await montaQuiz())
 })
 
-app.get('/genero/:id',cors(), async (req, res) =>{
+app.get('/genero/:id', cors(), async (req, res) =>{
     res.json(await montaQuiz(req.params.id))
 })
 
